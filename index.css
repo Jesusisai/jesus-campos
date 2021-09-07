d2a7ad8fc02c29f1ce963c58c217b2967336723d
Jesusisai <89815073+Jesusisai@users.noreply.github.com>
2021-09-07 17:45:17 -0500

    Update index.css

--- index.css
+++ index.css
@@ -1,1 +1,5 @@
-
+functiongetimc(){
+  var peso=parsefloat(document.getelementByid)(peso).value);
+  var altura=parsefloat(document.getelementByid)(altura).value);
+  var imc=peso/(altura+altura);
+  document.getelementByid(imc).inner html=imc;
